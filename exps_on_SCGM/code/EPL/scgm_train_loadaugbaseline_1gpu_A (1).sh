dc35a890418855b9756ded91805d9dfd93153f00
wqhIris <butflying@sina.cn>
2024-12-26 21:56:57 +0800

    codes for experiemnts on SCGM dataset

--- exps_on_SCGM/code/EPL/scgm_train_loadaugbaseline_1gpu_A (1).sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-now=$(date +"%Y%m%d_%H%M%S")
-
-save_path=exp/scgm_train_loadaugbaseline/1gpu/test_A
-mkdir -p $save_path
-
-python scgm_train_loadaugbaseline_1gpu_A.py 2>&1 | tee $save_path/$now.log
